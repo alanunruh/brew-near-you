db2627421e47d9e60529575ae9f7692b23d6854f
Alan Unruh <alan.unruh@gmail.com>
2017-04-23 20:33:53 +0000

    Adjusting them mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@brewnearyou.com"
   
-  def comment_added
-    mail(to: "alan.unruh@gmail.com",
-          subject: "A comment has been added to your place.")
+  def comment_added(comment)
+    @place = comment.place
+    @place_user = @place.user
+    mail(to: @place_user.email,
+          subject: "A comment has been added to #{@place.name}.")
   end
 end
